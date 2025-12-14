65770ef6f78de11eedbb616badcaf318c3ffe7ac
roneliyahu2001 <roneliyahu2001@gmail.com>
2025-12-14 13:09:00 +0200

    Initial commit

---
+++ src/Index2DTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Index2DTest {
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void distance2D() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
