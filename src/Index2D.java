6888f9cb6ad908acb94f86d4a284bff8082ffd19
roneliyahu2001 <roneliyahu2001@gmail.com>
2025-12-21 10:50:02 +0200

    Index 2D

--- src/Index2D.java
+++ src/Index2D.java
@@ -1,39 +1,49 @@
 public class Index2D implements Pixel2D {
+    private int x;
+    private int y;
+
     public Index2D(int w, int h) {
-        ;
+        this.x = w;
+        this.y = h;
     }
     public Index2D(Pixel2D other) {
-        ;
+
+        this.x = other.getX();
+        this.y = other.getY();
     }
+
     @Override
     public int getX() {
 
-        return 0;
+        return this.x;
     }
 
     @Override
     public int getY() {
 
-        return 0;
+        return this.y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
 
-        return 0;
+        int dx = this.x - p2.getX();
+        int dy = this.y - p2.getY();
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
     @Override
     public String toString() {
-        String ans = null;
-
-        return ans;
+        return "(" + this.x + "," + this.y + ")";
     }
 
     @Override
     public boolean equals(Object p) {
-        boolean ans = true;
+        if (this == p) return true;
+        if (p == null) return false;
+        if (!(p instanceof Pixel2D)) return false;
 
-        return ans;
+        Pixel2D other = (Pixel2D) p;
+        return this.x == other.getX() && this.y == other.getY();
     }
 }
--- src/Index2DTest.java
+++ src/Index2DTest.java
@@ -5,22 +5,81 @@ import static org.junit.jupiter.api.Assertions.*;
 class Index2DTest {
 
     @Test
-    void getX() {
+    void testGetX() {
+        Index2D p = new Index2D(3, 7);
+        int x = p.getX();
+        assertEquals(3, x);
     }
 
     @Test
-    void getY() {
+    public void testGetX2() {
+        Index2D p = new Index2D(-2, 5);
+        assertEquals(-2, p.getX());
     }
 
     @Test
-    void distance2D() {
+    void testGetY() {
+        Index2D p = new Index2D(3, 7);
+        int y = p.getY();
+        assertEquals(7, y);
+    }
+
+    @Test
+    void testGetY2() {
+        Index2D p = new Index2D(4, -9);
+        int y = p.getY();
+        assertEquals(-9, y);
+    }
+
+    @Test
+    void testDistance2D() {
+        Pixel2D p2 = new Index2D(1, 2);
+        Pixel2D p1 = new Index2D(4, 6);
+        double dist = p1.distance2D(p2);
+        assertEquals(5.0, dist);
+    }
+
+    @Test
+    void testDistance2D2() {
+        Pixel2D p1 = new Index2D(3, 7);
+        Pixel2D p2 = new Index2D(3, 7);
+
+        double dist = p1.distance2D(p2);
+
+        assertEquals(0.0, dist);
     }
 
     @Test
     void testToString() {
+        Index2D p = new Index2D(3, 7);
+        String str = p.toString();
+        assertEquals("(3,7)", str);
+    }
+
+    @Test
+    void testToString2() {
+        Index2D p = new Index2D(-2, -5);
+        String str = p.toString();
+        assertEquals("(-2,-5)", str);
     }
 
     @Test
     void testEquals() {
+        Index2D p = new Index2D(3, 7);
+        assertTrue(p.equals(p));
+    }
+
+    @Test
+    void testEquals2() {
+        Index2D p1 = new Index2D(3, 7);
+        Index2D p2 = new Index2D(3, 7);
+        assertTrue(p1.equals(p2));
+    }
+
+    @Test
+    void testEquals3() {
+        Index2D p1 = new Index2D(3, 7);
+        Index2D p2 = new Index2D(4, 7);
+        assertFalse(p1.equals(p2));
     }
 }
